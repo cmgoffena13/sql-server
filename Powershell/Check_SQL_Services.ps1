861abff4ddc3fa8d905234f8e090872ac83da292
Cortland Goffena <cortland.goffena@hotmail.com>
2023-03-10 16:45:47 -0600

    adding in powershell folder and services script

---
+++ Powershell/Check_SQL_Services.ps1
@@ -0,0 +1,20 @@
+$Servers = @('server1', 'server2', 'server3')
+
+foreach ($Computer in $Servers) {
+
+    Write-Host "`nChecking SQL Services Status for $Computer...`n"
+
+    Get-Service -ComputerName $Computer | Where-Object {$_.DisplayName -like 'SQL Server*'} | Sort-Object -Property 'DisplayName' | 
+    ForEach-Object {
+        if ($_.Status -eq 'Running') {
+            Write-Host -ForegroundColor 'Green' $_.DisplayName 'is running'
+        }
+        elseif ($_.Status -eq 'Stopped') {
+            Write-Host -ForegroundColor 'Red' $_.DisplayName 'is not running'
+        }
+        else {
+            Write-Host -ForegroundColor 'Yellow' $_.DisplayName 'has status: ' $_.Status
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # sql-server
 
 A collection of SQL Server scripts that I have developed. Most internet examples are poorly formatted and named.
-These are personal queries that I have written to assist myself in answering any questions I have while working with SQL Server. Most were written to provide more clarity around the state of objects and processes.
+These are personal queries and scripts that I have written to assist myself in answering any questions I have while working with SQL Server. Most were written to provide more clarity around the state of objects and processes.
