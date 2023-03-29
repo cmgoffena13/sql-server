4983d2c01f8e92de56541f8e5237aedef45e3d68
Cortland Goffena <cortland.goffena@hotmail.com>
2023-03-29 18:15:51 -0500

    adding in driver space script

---
+++ Powershell/Check_Driver_Space.ps1
@@ -0,0 +1,14 @@
+$Servers = @('server1', 'server2', 'server3')
+
+foreach ($Computer in $Servers) {
+
+Write-Output "`nChecking Driver Space for $Computer...`n"
+
+Get-CimInstance -ComputerName $Computer -ClassName Win32_LogicalDisk | 
+Select-Object -Property DeviceID, 
+                        @{Label='FreeSpaceGB'; Expression={ [int]($_.FreeSpace/1GB) }}, 
+                        @{Label='TotalSpaceGB'; Expression={ [int]($_.Size/1GB) }},
+                        @{Label='PercentFree'; Expression={ [math]::Round( ([int]($_.FreeSpace/1GB) / [int]($_.Size / 1GB)), 2) }} | 
+Format-Table -AutoSize
+
+}
